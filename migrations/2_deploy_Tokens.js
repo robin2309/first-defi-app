0d2a5f6bcc2006de6ae5ec4a1821c94a9751d1f4
robin2309 <robin.besson1@gmail.com>
2021-03-08 22:19:17 +0100

    first commit

---
+++ migrations/2_deploy_Tokens.js
@@ -0,0 +1,9 @@
+const MyToken = artifacts.require("MyToken");
+
+module.exports = async function (deployer, network, accounts) {
+  // Deploy MyToken
+
+  await deployer.deploy(MyToken);
+
+  const myToken = await MyToken.deployed();
+};
